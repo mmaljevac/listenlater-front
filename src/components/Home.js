5f1cd188417607128727fb20a598809effa638e2
Matija Maljevac <mmaljevac@neos.hr>
2023-11-22 18:46:17 +0100

    Delete album function

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,52 +7,63 @@ const Home = () => {
 
   const [albums, setAlbums] = useState([]);
 
-
-    useEffect(() => {
-      
-      const fetchData = async () => {
-        if (curUser) {
-        await fetch(
-          `http://localhost:8080/albums/getAlbumsByUser/${curUser.id}`,
-          {
-            method: "GET",
-          }
-        )
-          .then((response) => {
-            if (!response.ok) {
-              throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            console.log(response);
-            if (
-              response.status === 204 ||
-              response.headers.get("Content-Length") === "0"
-            ) {
-              return;
-            }
-  
-            return response.json();
-          })
-          .then((data) => {
-            console.log(data);
-            console.log("------");
-            if (data) {
-              setAlbums(data);
-            }
-            console.log(albums);
-          })
-          .catch((error) => {
-            console.error("Fetch error:", error);
-          });
-        } else {
-          return <Navigate to={{ pathname: "/login" }} />;
+  const fetchAlbums = async () => {
+    if (curUser) {
+      await fetch(
+        `http://localhost:8080/albums/getAlbumsByUser/${curUser.id}`,
+        {
+          method: "GET",
         }
-      };
-  
-      fetchData();
-    
-    }, []);
+      )
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+          console.log(response);
+          if (
+            response.status === 204 ||
+            response.headers.get("Content-Length") === "0"
+          ) {
+            return;
+          }
 
-  
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          console.log("------");
+          if (data) {
+            setAlbums(data);
+          }
+          console.log(albums);
+        })
+        .catch((error) => {
+          console.error("Fetch error:", error);
+        });
+    } else {
+      return <Navigate to={{ pathname: "/login" }} />;
+    }
+  };
+
+  useEffect(() => {
+    fetchAlbums();
+  }, []);
+
+  const handleDelete = async (id) => {
+    await fetch(`http://localhost:8080/albums/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        fetchAlbums();
+        return response.json();
+      })
+      .catch((error) => {
+        console.error("Fetch error:", error);
+      });
+  };
 
   return curUser ? (
     <div className="content">
@@ -60,12 +71,14 @@ const Home = () => {
       <ul>
         {albums.map((album) => (
           <li key={album.name}>
-            <a href={album.url}>
+            <a href={album.imgUrl}>
               <div>
-                <img src={album.imgUrl} /> <br/>
-                {album.name} <br/> {album.artist}
+                <img src={album.imgUrl} /> <br />
+                <p>{album.name}</p>
+                {album.artist}
               </div>
             </a>
+            <Link onClick={(e) => handleDelete(album.id)}>X</Link>
           </li>
         ))}
       </ul>
